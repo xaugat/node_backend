a0e5d227d88dff3aaa2ff456d5a6c42d8d64eea7
xaugat <sauugat@gmail.com>
2023-12-05 22:12:33 +0545

    connected FE and BE with api data shown

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+
+app.use(cors())
 
 app.get('/saugat', (req, res)=>{
     res.json({
@@ -9,7 +12,8 @@ app.get('/saugat', (req, res)=>{
         'last_name': 'poudel',
         'email': 'sauugat@gmail.com',
         'phone': '9860401034',
-        'address': 'Kathmandu'
+        'address': 'Kathmandu',
+        'profile_img': 'https://media.licdn.com/dms/image/D5603AQFnplXehPu-1A/profile-displayphoto-shrink_800_800/0/1673115786156?e=2147483647&v=beta&t=9m67SFBpHi1UFfXlRPiY2GgD4yReEh-taSI8PtDjB0s'
 
     })
 
